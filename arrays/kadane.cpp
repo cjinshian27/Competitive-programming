86dc914a497fec54bd4673eec7f5b0c87cf3a34c
Jin Shian <cjinshian77@usp.br>
2023-03-17 09:59:53 -0300

    code written for kadane.cpp

---
+++ arrays/kadane.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//kadane's algorithm for finding maximum subarray sum
+int kadane(vector<int> & numbers){
+	int bestSum = 0, sum = 0, n = numbers.size();
+	for(int i = 0; i < n; i++){
+		sum = max(numbers[i], numbers[i] + sum);
+		bestSum = max(sum, bestSum);
+	}
+	return bestSum;
+}	
