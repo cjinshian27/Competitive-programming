39f63e240b54e50b464fd735628ebca00f1767e7
Jin Shian <cjinshian77@usp.br>
2023-03-19 10:16:56 -0300

    changed kadane.cpp comment

--- arrays/kadane.cpp
+++ arrays/kadane.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//kadane's algorithm for finding maximum subarray sum
+//kadane's algorithm for finding maximum subarray sum with time complexity O(n)
 int kadane(vector<int> & numbers){
 	int bestSum = 0, sum = 0, n = numbers.size();
 	for(int i = 0; i < n; i++){
