5b7b9ba1df332141083503f5aeac969540e5faa5
Jin Shian <cjinshian77@usp.br>
2023-03-19 10:46:00 -0300

    tweaking binary_search.cpp

--- arrays/binary_search.cpp
+++ arrays/binary_search.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 //alternative binary search with time complexity O(log(n))
 void binary_search(vector<int> & numbers, int target){
-	int k = 0;
+	int k = 0, n = numbers.size();	
 	for(int b = n / 2; b >= 1; b = b / 2){
 		while(b + k < n && numbers[b + k] <= target){
 			k = k + b;
 		}
 	}	
 	if(numbers[k] == target){
-		return "target found";
+		cout << "target found" << '\n';
 	}
 	else{
-		return "target not found"
+		cout << "target not found" << '\n';
 	}
 }
